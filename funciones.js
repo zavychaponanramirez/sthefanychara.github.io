3af026ef0e5862507629d897c5f6aa05e9a5a05b
zavychaponanramirez <81723179+zavychaponanramirez@users.noreply.github.com>
2021-05-22 19:47:07 -0500

    ja

--- funciones.js
+++ funciones.js
@@ -1,6 +1,2 @@
 
 //aparecera una ventana popat
-document.getElementById('mensaje').innerHTML="Certus";
-document.write("<div style='background-color: #776199;">Bienvenido</div>)
-window.alert('Bienvenido a Certus');
-console.log('Mensaje en la consola del navegador')
