3578a56bf77b1d286f85ebc3bf8468dbd2d6f0fb
zavychaponanramirez <81723179+zavychaponanramirez@users.noreply.github.com>
2021-05-28 21:51:38 -0500

    loe

--- funciones.js
+++ funciones.js
@@ -1,7 +1,37 @@
 
-//aparecera una ventana popat
-var dato;
+var _valor1=10;
+var $valor2=30;
+var resultado=_valor1 + $valor2;
+var aprobado=10;
 
-function calcularEdad(params){
-    var 
+var calcularSuma=function name(a, b){
+    return a + b;
+}
+var obj={
+    nombre="Dato",
+}
+console.alert('Aperturado')
+
+function ModificarDato(){
+    document.getElementById('resultado').innerHTML='<button>valor</button>',
+    calcularSuma(10, 3),
+};
+function pintado(){
+    aprobado = true; //booleano
+    document.getElementById('mensaje').innerHTML = 'scrolling';
+};
+function Holas(){
+    document.getElementById('Hola').innerHTML='Bienvenido O\'Conn'\'
+    
+}
+
+function compara2(){
+    var bandera=false;
+    var numero3=document.getElementById('numero3').value;
+    var numero4=documen.getElementById('numero4').value;
+     
+    if(numero3 < numero4){
+        bandera=true;
+    }
+    document.getElementById('respuest2').innerHTML=bandera
 }
